bbccba1e5dee57dd282e260f9021f8cd64b7b871
John William Seno <jwpseno@gmail.com>
2017-09-26 07:22:24 +0800

    initial setup

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,42 @@
+import React, {PropTypes} from 'react';
+import { connect} from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as courseActions from '../../actions/courseActions';
+
+class CoursesPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
+      </div>
+    );
+  }
+}
+
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
