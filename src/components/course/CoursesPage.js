d7db03c2dda278abfbe5a82cc61441503b82ed9b
John William P. Seno <jwpseno@microsourcing.local>
2017-09-27 10:03:24 +0800

    updated with async lessons

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,21 +2,35 @@ import React, {PropTypes} from 'react';
 import { connect} from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
+import { browserHistory } from 'react-router';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
   }
 
   courseRow(course, index) {
     return <div key={index}>{course.title}</div>;
   }
 
+  redirectToAddCoursePage() {
+    browserHistory.push('/course');
+  }
+
   render() {
+    const {courses} = this.props;
+
     return (
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
+        <input 
+          type="submit"
+          value="Add Course"
+          className="btn btn-primary"
+          onClick={this.redirectToAddCoursePage} />
+        <CourseList courses={courses} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import { loadCourses } from './actions/courseActions';
+import { loadAuthors } from './actions/authorActions';
 import './styles/styles.css'; // Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
+store.dispatch(loadCourses());
+store.dispatch(loadAuthors());
 
 render(
   <Provider store={store}>
